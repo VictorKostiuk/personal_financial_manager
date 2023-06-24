1464cf363d870ba42b4d82a600dc2ea127664d9f
Viktor Kostiuk <vityok.kostiuk@gmail.com>
2023-06-24 12:42:52 +0300

    Added validations for models

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,3 +1,6 @@
 class Operation < ApplicationRecord
+  validates :amount, presence: true, numericality: { other_than: 0 }
+  validates :description, presence: true
+  validates :odate, presence: true
   belongs_to :category
 end
